deda70975da04ba89dd2595573a37a84849d81bb
Joris Boelen <jboelen@tux-inside.com>
2024-09-03 18:26:31 +0200

    Bugfix username in menu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule, Location } from '@angular/common';
+import { CommonModule, Location, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AudioPlayerComponent } from './components/audio-player/audio-player.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,7 +8,7 @@ import { ToastNotificationComponent } from './components/toast-notification/toas
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AudioPlayerComponent, CommonModule, RouterOutlet, NavbarComponent, ToastNotificationComponent],
+  imports: [AudioPlayerComponent, CommonModule, RouterOutlet, NavbarComponent, NgIf, ToastNotificationComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
